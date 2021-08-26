c350265d1789587c5b0f009e849b1859dc59edf4
lnuss2 <89098726+lnuss2@users.noreply.github.com>
2021-08-26 18:47:39 -0500

    Collaboration changes.

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Ali Keshk
+ * Date: 2021/08/25
+*/
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Ali Keshk");
+		System.out.println("Computer Science");
+		System.out.println("Landon Nuss");
+		System.out.println("Computer Science");
+	}
+
+}
