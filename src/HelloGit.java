4d78fa07215d1cc8bda8d5718fbed17fc8571a63
lnuss2 <89098726+lnuss2@users.noreply.github.com>
2021-08-27 08:44:39 -0500

    Prints name and major.

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,11 +1,13 @@
+/**
+ * Author: Landon Nuss
+ * Date: 8/27/2021
+*/
 public class HelloGit {
 
     public static void main(String[] args) {
 
     	System.out.println("Landon Nuss");
         System.out.println("Computer Science!");
-        System.out.println("Ali Keshk");
-        System.out.println("Computer Science");
 
     }
 
